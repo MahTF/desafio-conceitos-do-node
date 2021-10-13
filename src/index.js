e38fa243de3a502152dbed3bdb8907338c43cbd4
Marcelo Ferreira <mah.tf@hotmail.com>
2021-10-13 09:10:33 -0300

    Created putch/todos/:id/done

--- src/index.js
+++ src/index.js
@@ -84,7 +84,18 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const task = user.todos.find((task) => task.id === id);
+
+  if (!task) {
+    return response.status(404).json({ error: "Task not found!" });
+  }
+
+  task.done = true;
+
+  return response.json(task);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
